56a277b55cd190f350ec2c04a61e15ec3440bef4
Kultida11 <s6607012663034@email.kmutnb.ac.th>
2024-02-28 14:24:07 +0700

    NextJS Project

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
+import { inter } from '@/app/ui/fonts';
  
-export const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: {
@@ -9,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
